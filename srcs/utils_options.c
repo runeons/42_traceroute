7c5412973cf474ffab449dd9fcc154ea8ca28956
ts <ts@ts.com>
2023-12-18 12:12:02 -0600

    add w option

--- srcs/utils_options.c
+++ srcs/utils_options.c
@@ -5,7 +5,7 @@ t_option allowed_options[] =
     {'h', "help",           0, "",          NULL, "give this help list"},
     {'q', "nqueries",       1, "N",         NULL, "send N probes per hop"}, // default 3
     {'m', "max-hops",       1, "N",         NULL, "specify N as max time-to-live / max hop to reach"}, // default 30
-    // {'w', "wait",           1, "MAX",       NULL, "wait MAX seconds between sending each packet"}, // default 5
+    {'w', "wait",           1, "MAX",       NULL, "wait MAX seconds between sending each packet"}, // default 5
     // {'p', "port",           1, "PORT",      NULL, "specify PORT as the starting destination port"},  // default 33434
     // {'s', "source",         1, "SRC",       NULL, "send outgoing probes from SRC address"},
     // {'z', "sendwait",       1, "N",         NULL, "wait at least N seconds between sending each probe"}, // default 0
